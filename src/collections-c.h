938986d47d5a785a594304e4c0cf415fbbbbf2a6
srdja <srdjan.panic@ymail.com>
2014-08-19 13:43:57 +0200

    grouping all headers into collections-c.h

---
+++ src/collections-c.h
@@ -0,0 +1,20 @@
+/*
+ * collections-c.h
+ *
+ *  Created on: Aug 19, 2014
+ *      Author: srdja
+ */
+
+#ifndef COLLECTIONS_C_H_
+#define COLLECTIONS_C_H_
+
+#include "hashtable.h"
+#include "dlist.h"
+#include "hashset.h"
+#include "vector.h"
+#include "stack.h"
+#include "queue.h"
+#include "slist.h"
+#include "deque.h"
+
+#endif /* COLLECTIONS_C_H_ */
