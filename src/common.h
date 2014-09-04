d50dafbda586b047cd8f9e6953b4a0d48bbde880
srdja <srdjan.panic@ymail.com>
2014-09-05 01:53:58 +0200

    adding a common header for all standard lib includes

---
+++ src/common.h
@@ -0,0 +1,16 @@
+/*
+ * common.h
+ *
+ *  Created on: Sep 3, 2014
+ *      Author: srdja
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+#endif /* COMMON_H_ */
