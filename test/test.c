a65f2c35714db2612c058bc0eeebe3be0d359606
srdja <srdjan.panic@ymail.com>
2014-09-11 02:24:42 +0200

    addding a minimal testing framework

--- test/test.c
+++ test/test.c
@@ -1,21 +1,44 @@
-/*
- * test.c
- *
- *  Created on: Jul 7, 2014
- *      Author: srdja
- */
+#include "test.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../src/dlist.h"
-#include <assert.h>
-#include "../src/collections-c.h"
-#include "hashtable_test.h"
+int test_status = PASS;
+int exit_on_failure = false;
 
-int main(int argc, char **argv)
+const size_t msg_size = 256;
+char msg_buff[256];
+
+void cc_assert(bool exp, char *msg)
 {
-    //dlist_test_init();
-    test_hashtable_init();
-    return 0;
+	 if (!exp) {
+		  test_status = FAIL;
+		  printf("%s\n", msg);
+		  if (exit_on_failure)
+			   exit(FAIL);
+	 }
+}
+
+char *cc_msg(char *format, ...)
+{
+	 va_list args; 
+	 va_start(args, format);
+	 
+	 vsnprintf(msg_buff, msg_size, format, args);
+	 
+	 va_end(args);
+	 return msg_buff;
+}
+
+
+void cc_set_status(int status)
+{
+    test_status = status;
+}
+
+void cc_set_exit_on_failure(bool exit)
+{
+    exit_on_failure = exit;
+}
+
+int cc_get_status()
+{
+    return test_status;
 }
