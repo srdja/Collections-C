d859222e4038d59b48c0191393243ba7615a9286
srdja <srdjan.panic@ymail.com>
2014-09-23 14:28:44 +0200

    defining INLINE

--- src/common.h
+++ src/common.h
@@ -15,4 +15,16 @@
 
 #define NO_SUCH_INDEX (size_t) - 1
 
+#if defined(_MSC_VER)
+
+#define INLINE __inline
+#define FORCE_INLINE __forceinline
+
+#else
+
+#define INLINE inline
+#define FORCE_INLINE inline __attribute__((always_inline))
+
+#endif /* _MSC_VER */
+
 #endif /* COMMON_H_ */
