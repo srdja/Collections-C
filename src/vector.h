ba4af70d5afc27f6b5d52fc2a956d5cd43706ced
srdja <srdjan.panic@ymail.com>
2014-11-08 23:04:00 +0100

    reordering the parameters to make them consistent with other interfaces and beautifying

--- src/vector.h
+++ src/vector.h
@@ -23,51 +23,51 @@
 
 #include "common.h"
 
-typedef struct vector_s Vector;
+typedef struct vector_s      Vector;
 typedef struct vector_iter_s VectorIter;
 
-Vector *vector_new();
-Vector *vector_new_capacity(size_t capacity);
+Vector*     vector_new             ();
+Vector*     vector_new_capacity    (size_t capacity);
 
-void vector_destroy(Vector *vect);
-void vector_destroy_free(Vector *vect);
+void        vector_destroy         (Vector *vect);
+void        vector_destroy_free    (Vector *vect);
 
-bool vector_add(Vector *vect, void *element);
-bool vector_add_at(Vector *vect, size_t index, void *element);
-void *vector_replace_at(Vector *vect, size_t index, void *element);
+bool        vector_add             (Vector *vect, void *element);
+bool        vector_add_at          (Vector *vect, void *element, size_t index);
+void*       vector_replace_at      (Vector *vect, void *element, size_t index);
 
-void *vector_remove(Vector *vect, void *element);
-void *vector_remove_at(Vector *vect, size_t index);
-void *vector_remove_last(Vector *vect);
-void vector_remove_all(Vector *vect);
-void vector_remove_all_free(Vector *vect);
+void*       vector_remove          (Vector *vect, void *element);
+void*       vector_remove_at       (Vector *vect, size_t index);
+void*       vector_remove_last     (Vector *vect);
+void        vector_remove_all      (Vector *vect);
+void        vector_remove_all_free (Vector *vect);
 
-void *vector_get(Vector *vect, size_t index);
-void *vector_get_last(Vector *vect);
+void*       vector_get             (Vector *vect, size_t index);
+void*       vector_get_last        (Vector *vect);
 
-Vector *vector_subvector(Vector *vect, size_t from, size_t to);
-Vector *vector_copy_shallow(Vector *vect);
-Vector *vector_copy_deep(Vector *vect, void *(*cp) (void*));
+Vector*     vector_subvector       (Vector *vect, size_t from, size_t to);
+Vector*     vector_copy_shallow    (Vector *vect);
+Vector*     vector_copy_deep       (Vector *vect, void *(*cp) (void*));
 
-void vector_reverse(Vector *vect);
-void vector_trim_capacity(Vector *vect);
+void        vector_reverse         (Vector *vect);
+void        vector_trim_capacity   (Vector *vect);
 
-int vector_cointains(Vector *vect, void *element);
-size_t vector_size(Vector *vect);
-size_t vector_capacity(Vector *vect);
+int         vector_cointains       (Vector *vect, void *element);
+size_t      vector_size            (Vector *vect);
+size_t      vector_capacity        (Vector *vect);
 
-int vector_index_of(Vector *vect, void *element);
-void vector_sort(Vector *vect, int (*cmp) (const void*, const void*));
+int         vector_index_of        (Vector *vect, void *element);
+void        vector_sort            (Vector *vect, int (*cmp) (const void*, const void*));
 
-void vector_foreach(Vector *vect, void (*op) (void *));
+void        vector_foreach         (Vector *vect, void (*op) (void *));
 
-VectorIter *vector_iter_new(Vector *vect);
-void vector_iter_destroy(VectorIter *iter);
-bool vector_iter_has_next(VectorIter *iter);
-void *vector_iter_next(VectorIter *iter);
-void *vector_iter_remove(VectorIter *iter);
-void vector_iter_add(VectorIter *iter, void *element);
-void *vector_iter_replace(VectorIter *iter, void *element);
-size_t vector_iter_index(VectorIter *iter);
+VectorIter* vector_iter_new        (Vector *vect);
+void        vector_iter_destroy    (VectorIter *iter);
+bool        vector_iter_has_next   (VectorIter *iter);
+void*       vector_iter_next       (VectorIter *iter);
+void*       vector_iter_remove     (VectorIter *iter);
+void        vector_iter_add        (VectorIter *iter, void *element);
+void*       vector_iter_replace    (VectorIter *iter, void *element);
+size_t      vector_iter_index      (VectorIter *iter);
   
 #endif /* VECTOR_H_ */
