8b3d1428770152ff0073a07f26b6d34344f2fc29
srdja <srdjan.panic@ymail.com>
2014-12-29 05:39:00 +0100

    adding 64 bit hash function variants

--- src/hashtable.c
+++ src/hashtable.c
@@ -835,13 +835,20 @@ void *hashtable_iter_get_value(HashTableIter *iter)
     return iter->prev_entry->value;
 }
 
-/**
- * Slightly modified djb2 string hashing function
- */
-uint32_t hashtable_hash_string(const void *key, int len, uint32_t seed)
+
+
+/*******************************************************************************
+ *
+ *
+ *  djb2 string hash
+ *
+ *
+ ******************************************************************************/
+
+size_t hashtable_hash_string(const void *key, int len, size_t seed)
 {
-    const char *str = key;
-    register uint32_t hash = 5381;
+    const    char   *str  = key;
+    register size_t  hash = 5381;
 
     while (*str++)
         hash = ((hash << 5) + hash) ^ *str;
@@ -851,27 +858,195 @@ uint32_t hashtable_hash_string(const void *key, int len, uint32_t seed)
 
 /*******************************************************************************
  *
- * MurmurHash3 by Austin Appleby, adapted for hashtable use.
+ *
+ *  MurmurHash3 by Austin Appleby, adapted for hashtable use.
+ *
  *
  ******************************************************************************/
 
-#if defined(_MSC_VER)
+#ifdef _MSC_VER
 
 #define ROTL32(x,y) _rotl(x,y)
+#define ROTL64(x,y)	_rotl64(x,y)
+#define BIG_CONSTANT(x) (x)
 
-#else   // defined(_MSC_VER)
+#else
 
-inline uint32_t rotl32 (uint32_t x, int8_t r)
+inline uint32_t rotl32(uint32_t x, int8_t r)
 {
     return (x << r) | (x >> (32 - r));
 }
 
+inline uint64_t rotl64(uint64_t x, int8_t r)
+{
+    return (x << r) | (x >> (64 - r));
+}
+
 #define ROTL32(x,y) rotl32(x,y)
+#define ROTL64(x,y)	rotl64(x,y)
+#define BIG_CONSTANT(x) (x##LLU)
 
-#endif // !defined(_MSC_VER)
+#endif
 
-//-----------------------------------------------------------------------------
-// Finalization mix - force all bits of a hash block to avalanche
+
+/*****************************************************************************
+ *
+ *                            -- 64bit --
+ * 
+ ****************************************************************************/
+#ifdef ARCH_64
+
+
+FORCE_INLINE uint64_t fmix64(uint64_t k)
+{
+    k ^= k >> 33;
+    k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+    k ^= k >> 33;
+    k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+    k ^= k >> 33;
+
+    return k;
+}
+
+uint64_t hashtable_hash(const void *key, int len, uint32_t seed)
+{
+    const uint8_t  *data    = (const uint8_t*) key;
+    const int       nblocks = len / 16;
+
+    uint64_t        h1      = seed;
+    uint64_t        h2      = seed;
+
+    const uint64_t  c1      = BIG_CONSTANT(0x87c37b91114253d5);
+    const uint64_t  c2      = BIG_CONSTANT(0x4cf5ad432745937f);
+
+    const uint64_t *blocks  = (const uint64_t*)(data);
+
+    for(int i = 0; i < nblocks; i++) {
+        uint64_t k1 = blocks[i*2+0];
+        uint64_t k2 = blocks[i*2+1];
+
+        k1 *= c1;
+        k1  = ROTL64(k1,31);
+        k1 *= c2;
+        h1 ^= k1;
+        h1  = ROTL64(h1,27);
+        h1 += h2;
+        h1  = h1 * 5 + 0x52dce729;
+        
+        k2 *= c2;
+        k2  = ROTL64(k2,33);
+        k2 *= c1;
+        h2 ^= k2;
+        h2  = ROTL64(h2,31);
+        h2 += h1;
+        h2  = h2 * 5 + 0x38495ab5;
+    }
+    
+    const uint8_t *tail = (const uint8_t*)(data + nblocks*16);
+
+    uint64_t k1 = 0;
+    uint64_t k2 = 0;
+
+    switch(len & 15) {
+    case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+    case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+    case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+    case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+    case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+    case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
+    case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
+             k2 *= c2;
+             k2  = ROTL64(k2,33);
+             k2 *= c1;
+             h2 ^= k2;
+
+    case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
+    case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
+    case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
+    case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
+    case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
+    case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
+    case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
+    case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
+             k1 *= c1;
+             k1  = ROTL64(k1,31);
+             k1 *= c2;
+             h1 ^= k1;
+    };
+
+    h1 ^= len; h2 ^= len;
+
+    h1 += h2;
+    h2 += h1;
+
+    h1 = fmix64(h1);
+    h2 = fmix64(h2);
+
+    h1 += h2;
+    h2 += h1;
+
+    return h1;
+}
+
+/*
+ * MurmurHash3 the 64bit variant that hashes the pointer itself
+ */
+uint64_t hashtable_hash_ptr(const void *key, int len, uint32_t seed)
+{
+    const int nblocks = len / 4;
+
+    uint64_t h1 = seed;
+    uint64_t h2 = seed;
+
+    const uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
+    const uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
+
+    int i;
+    for (i = 0; i < nblocks; i++) {
+        uint64_t k1 = ((uintptr_t) key >> (2 * i)) & 0xff;
+        uint64_t k2 = ROTL64(k1, 13);
+
+        k1 *= c1;
+        k1  = ROTL64(k1,31);
+        k1 *= c2;
+        h1 ^= k1;
+        h1  = ROTL64(h1,27);
+        h1 += h2;
+        h1  = h1 * 5 + 0x52dce729;
+  
+        k2 *= c2;
+        k2  = ROTL64(k2,33);
+        k2 *= c1;
+        h2 ^= k2;
+        h2  = ROTL64(h2,31);
+        h2 += h1;
+        h2  = h2 * 5 + 0x38495ab5;
+    }
+
+    /* Since the pointers are power of two length 
+     * we don't need a tail mix */
+    
+    h1 ^= len; h2 ^= len;
+
+    h1 += h2;
+    h2 += h1;
+
+    h1 = fmix64(h1);
+    h2 = fmix64(h2);
+
+    h1 += h2;
+    h2 += h1;
+
+    return h1;   
+}
+
+
+/*****************************************************************************
+ *
+ *                            -- 32bit --
+ * 
+ ****************************************************************************/
+#else
 
 FORCE_INLINE uint32_t fmix32(uint32_t h)
 {
@@ -884,19 +1059,19 @@ FORCE_INLINE uint32_t fmix32(uint32_t h)
     return h;
 }
 
-uint32_t hashtable_murmur_hash3(const void *key, int len, uint32_t seed)
+/**
+ * MurmurHash3 the 32bit variant.
+ */
+uint32_t hashtable_hash(const void *key, int len, uint32_t seed)
 {
-    const uint8_t *data = (const uint8_t*)key;
-    const int nblocks = len / 4;
+    const uint8_t *data    = (const uint8_t*)key;
+    const int      nblocks = len / 4;
 
     uint32_t h1 = seed;
 
     const uint32_t c1 = 0xcc9e2d51;
     const uint32_t c2 = 0x1b873593;
-
-    //----------
-    // body
-
+    
     const uint32_t *blocks = (const uint32_t *)(data + nblocks*4);
 
     int i;
@@ -912,9 +1087,6 @@ uint32_t hashtable_murmur_hash3(const void *key, int len, uint32_t seed)
         h1 = h1*5+0xe6546b64;
     }
 
-    //----------
-    // tail
-
     const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
 
     uint32_t k1 = 0;
@@ -923,24 +1095,22 @@ uint32_t hashtable_murmur_hash3(const void *key, int len, uint32_t seed)
     case 3: k1 ^= tail[2] << 16;
     case 2: k1 ^= tail[1] << 8;
     case 1: k1 ^= tail[0];
-            k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+            k1 *= c1;
+            k1  = ROTL32(k1,15);
+            k1 *= c2;
+            h1 ^= k1;
     };
 
-    //----------
-    // finalization
-
     h1 ^= len;
-
-    h1 = fmix32(h1);
+    h1  = fmix32(h1);
 
     return h1;
 }
 
-/**  
- * 
- *
+/*
+ * MurmurHash3 the 32bit variant that hashes the pointer itself
  */
-uint32_t hashtable_murmur_hash3_pointer_hash(const void *key, int len, uint32_t seed)
+uint32_t hashtable_hash_ptr(const void *key, int len, uint32_t seed)
 { 
     const int nblocks = len / 4;
 
@@ -951,7 +1121,7 @@ uint32_t hashtable_murmur_hash3_pointer_hash(const void *key, int len, uint32_t 
 
     int i;
     for (i = 0; i < nblocks; i++) {
-        uint32_t k1 = ((uintptr_t) key >> (32*i)) & 0xff;
+        uint32_t k1 = ((uintptr_t) key >> (2*i)) & 0xff;
 
         k1 *= c1;
         k1 = ROTL32(k1,15);
@@ -961,11 +1131,14 @@ uint32_t hashtable_murmur_hash3_pointer_hash(const void *key, int len, uint32_t 
         h1 = ROTL32(h1,13);
         h1 = h1*5+0xe6546b64;
     }
-    
-    // tail - no byte tails when dealing with power of 2 lenghts
 
+    /* Since the pointers are power of two length 
+     * we don't need a tail mix */
+    
     h1 ^= len;
-    h1 = fmix32(h1);
+    h1  = fmix32(h1);
 
     return h1;
 }
+
+#endif /* ARCH_64 */
