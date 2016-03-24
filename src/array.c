88bae0362194fc6131b3f950ae210d1159cfb3b6
srdja <i@srdja.me>
2016-03-24 17:44:46 +0100

    Fixing an integer overflow when the array size is 0

--- src/array.c
+++ src/array.c
@@ -368,6 +368,9 @@ enum cc_stat array_get_at(Array *ar, size_t index, void **out)
  */
 enum cc_stat array_get_last(Array *ar, void **out)
 {
+    if (ar->size == 0)
+        return CC_ERR_OUT_OF_RANGE;
+
     return array_get_at(ar, ar->size - 1, out);
 }
 
