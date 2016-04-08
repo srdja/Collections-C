9013a70beb1aafe6273d5301860c47409c3ae156
srdja <i@srdja.me>
2016-04-08 05:24:52 +0200

    Updating queue tests

--- test/queue_test.c
+++ test/queue_test.c
@@ -4,6 +4,8 @@
 void test_queue_poll();
 void test_queue_enqueue();
 void test_queue_iter();
+void test_queue_zip_iter();
+
 
 int main(int argc, char **argv)
 {
@@ -12,6 +14,7 @@ int main(int argc, char **argv)
     test_queue_poll();
     test_queue_enqueue();
     test_queue_iter();
+    test_queue_zip_iter();
 
     return cc_get_status();
 }
@@ -118,4 +121,60 @@ void test_queue_iter()
                      "by the iterator."));
 
     queue_destroy(q);
+}
+
+
+void test_queue_zip_iter_next()
+{
+    Queue *q1;
+    queue_new(&q1);
+
+    queue_enqueue(q1, "a");
+    queue_enqueue(q1, "b");
+    queue_enqueue(q1, "c");
+    queue_enqueue(q1, "d");
+
+    Queue *q2;
+    queue_new(&q2);
+
+    queue_enqueue(q2, "e");
+    queue_enqueue(q2, "f");
+    queue_enqueue(q2, "g");
+
+    QueueZipIter zip;
+    queue_zip_iter_init(&zip, q1, q2);
+
+    size_t i = 0;
+
+    void *e1, *e2;
+    while (queue_zip_iter_next(&zip, &e1, &e2) != CC_ITER_END) {
+        if (i == 0) {
+            cc_assert(strcmp((char*) e1, "d") == 0,
+                      cc_msg("queue_zip_iter_next: Expected e1 was \"a\" at index 0, but got %s instead",
+                             (char*) e1));
+            cc_assert(strcmp((char*) e2, "g") == 0,
+                      cc_msg("queue_zip_iter_next: Expected e1 was \"e\" at index 0, but got %s instead",
+                             (char*) e2));
+        }
+        if (i == 2) {
+            cc_assert(strcmp((char*) e1, "b") == 0,
+                      cc_msg("queue_zip_iter_next: Expected e1 was \"a\" at index 2, but got %s instead",
+                             (char*) e1));
+            cc_assert(strcmp((char*) e2, "e") == 0,
+                      cc_msg("queue_zip_iter_next: Expected e1 was \"e\" at index 2, but got %s instead",
+                             (char*) e2));
+        }
+        i++;
+    }
+    cc_assert(i == 3,
+              cc_msg("queue_zip_iter_next: Expected 3 iterations, but got %d instead", i));
+
+    queue_destroy(q1);
+    queue_destroy(q2);
+}
+
+
+void test_queue_zip_iter()
+{
+    test_queue_zip_iter_next();
 }
