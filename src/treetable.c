2f8caaea27415b443936d19562f964164bd5421b
srdja <i@srdja.me>
2016-04-11 09:39:02 +0200

    Adding missing TreeTableConf comparator function initialization

--- src/treetable.c
+++ src/treetable.c
@@ -65,6 +65,7 @@ void treetable_conf_init(TreeTableConf *conf)
     conf->mem_alloc  = malloc;
     conf->mem_calloc = calloc;
     conf->mem_free   = free;
+    conf->cmp        = cc_common_cmp_ptr;
 }
 
 /**
