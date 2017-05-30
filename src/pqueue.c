034eb51b51aaac9ca0e997d2ebe76dd4c6163aeb
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2017-05-30 13:46:00 +0530

    Adding push, pop, top functions of heap

--- src/pqueue.c
+++ src/pqueue.c
@@ -38,6 +38,10 @@ struct pqueue_s {
 };
 
 static int cc_default_comparator(const void *a, const void *b);
+static size_t cc_parent(size_t a);
+static void cc_heapify(PQueue *pqueue, size_t index);
+static size_t cc_left(size_t a);
+static size_t cc_right(size_t a);
 
 
 /*
@@ -142,6 +146,72 @@ void pqueue_destroy_free(PQueue *pqueue)
     pqueue->mem_free(pqueue);
 }
 
+/*
+ * Pushes the element in the pqueue
+ * @param[in] pqueue the priority queue in which the element is to be pushed
+ * @param[in] element the element which is needed to be pushed
+ *
+ * @return CC_OK if the element was successfully pushed, or CC_ERR_ALLOC
+ * if the memory allocation for the new element failed.
+ */
+
+enum cc_stat pqueue_push(PQueue *pqueue, void *element)
+{
+
+    int i = array_size(pqueue->v);
+    void *child, *parent;
+    enum cc_stat status = array_add(pqueue->v, element);
+    if(status == CC_ERR_ALLOC)
+        return status;
+    if(i == 0)
+        return CC_OK;
+    array_get_at(pqueue->v, i, &child);
+    array_get_at(pqueue->v, cc_parent(i), &parent);
+    while(i != 0 && pqueue->comparator(child, parent) > 0)
+    {
+        array_swap_at(pqueue->v, i, cc_parent(i));
+        i = cc_parent(i);
+        array_get_at(pqueue->v, i, &child);
+        array_get_at(pqueue->v, cc_parent(i), &parent);
+    }
+    return CC_OK;
+}
+
+/*
+ * Gets the most prioritized element from the queue without popping it
+ * @param[in] pqueue the PQueue structure of which the top element is needed
+ * @param[out] out pointer where the element is stored
+ *
+ * @return CC_OK if the element was found, or CC_ERR_VALUE_NOT_FOUND if the
+ * PQueue is empty.
+ */
+
+enum cc_stat pqueue_top(PQueue *pqueue, void **out)
+{
+    return array_get_at(pqueue->v, 0, out);
+}
+
+/*
+ * Removes the most prioritized element from the PQueue
+ * @param[in] pqueue the PQueue structure whose element is needed to be popped
+ * @param[out] out the pointer where the removed element will be stored
+ *
+ * return CC_OK if the element was popped successfully, or CC_ERR_OUT_OF_RANGE
+ * if pqueue was empty
+ */
+
+enum cc_stat pqueue_pop(PQueue *pqueue, void **out)
+{
+    void *tmp;
+    if(array_size(pqueue->v) == 0)
+        return CC_ERR_OUT_OF_RANGE;
+    array_swap_at(pqueue->v, 0, array_size(pqueue->v) - 1);
+    array_remove_last(pqueue->v, &tmp);
+    cc_heapify(pqueue, 0);
+    if(out)
+        *out = tmp;
+    return CC_OK;
+}
 
 /*
  * A default comparator function for the ints.
@@ -154,4 +224,71 @@ void pqueue_destroy_free(PQueue *pqueue)
 static int cc_default_comparator(const void *a, const void *b)
 {
     return *((int *) a) - *((int *) b);
+}
+
+/*
+ * A function used to find the parent of the node number i.
+ * @param[in] a, the index of node in the heap
+ * @return a non-negative integer, which is the index of the parent of node i
+ */
+
+static size_t cc_parent(size_t a)
+{
+    return (a - 1) / 2;
+}
+
+/*
+ * A function used to find the left child of the node number i.
+ * @param[in] a, the index of node in the heap
+ * @return a non-negative integer, which is the index of the left child of node i
+ */
+
+static size_t cc_left(size_t a)
+{
+    return 2 * a + 1;
+}
+
+/*
+ * A function used to find the right child of the node number i.
+ * @param[in] a, the index of node in the heap
+ * @return a non-negative integer, which is the index of the right child of node i
+ */
+
+static size_t cc_right(size_t a)
+{
+    return 2 * a + 2;
+}
+
+/*
+ * Function to maintain the heap property of the PQueue
+ * @param[in] pqueue the PQueue structure whose heap property is to be maintained
+ * @param[in] index the index from where we need to apply this operation
+ */
+
+static void cc_heapify(PQueue *pqueue, size_t index)
+{
+    if(array_size(pqueue->v) == 1 || array_size(pqueue->v) == 0)
+        return;
+    void *left, *right, *indexPtr;
+    size_t L = cc_left(index), R = cc_right(index), tmp = index;
+    array_get_at(pqueue->v, index, &indexPtr);
+    array_get_at(pqueue->v, L, &left);
+    array_get_at(pqueue->v, R, &right);
+    if(L >= array_size(pqueue->v) || R >= array_size(pqueue->v))
+        return;
+    if(pqueue->comparator(indexPtr, left) < 0) {
+        indexPtr = left;
+        index = L;
+    }
+    if(pqueue->comparator(indexPtr, right) < 0) {
+        indexPtr = right;
+        index = R;
+    }
+    if(index == tmp)
+        return;
+    else
+    {
+        array_swap_at(pqueue->v, index, tmp);
+        cc_heapify(pqueue, index);
+    }
 }
