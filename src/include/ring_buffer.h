1cb02190a2a6d3f758012354bcfab2a4e45e7f71
A Silent Cat <silentcat@protonmail.com>
2019-03-09 14:10:37 -0600

    Made a few changes

--- src/include/ring_buffer.h
+++ src/include/ring_buffer.h
@@ -3,7 +3,7 @@
  * @Date:   2019-03-07T10:32:52-06:00
  * @Email:  silentcat@protonmail.com
  * @Last modified by:   silentcat
- * @Last modified time: 2019-03-07T22:50:44-06:00
+ * @Last modified time: 2019-03-09T13:20:45-06:00
  */
 
 #ifndef COLLECTIONS_C_RBUF_H
@@ -22,24 +22,14 @@ typedef struct ring_buffer_conf {
 
 } RbufConf;
 
-typedef struct ring_buffer {
-
-    size_t size, capacity;
-    int head, tail;
-    uint64_t *buf;
-
-    void *(*mem_alloc)  (size_t size);
-    void *(*mem_calloc) (size_t blocks, size_t size);
-    void  (*mem_free)   (void *block);
-
-} Rbuf;
+typedef struct ring_buffer Rbuf;
 
 enum cc_stat  rbuf_new           (Rbuf **rbuf);
-enum cc_stat  rbuf_conf_init     (RbufConf *rconf);
+void          rbuf_conf_init     (RbufConf *rconf);
 enum cc_stat  rbuf_conf_new      (RbufConf *rconf, Rbuf **rbuf);
 enum cc_stat  rbuf_enqueue       (Rbuf *rbuf, uint64_t item);
 enum cc_stat  rbuf_dequeue       (Rbuf *rbuf, uint64_t *out);
-int           rbuf_is_empty      (Rbuf *rbuf);
+bool          rbuf_is_empty      (Rbuf *rbuf);
 void          rbuf_destroy       (Rbuf *rbuf);
 
 #endif
--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -3,11 +3,22 @@
  * @Date:   2019-03-07T10:32:56-06:00
  * @Email:  silentcat@protonmail.com
  * @Last modified by:   silentcat
- * @Last modified time: 2019-03-07T22:58:26-06:00
+ * @Last modified time: 2019-03-09T14:10:11-06:00
  */
 
 #include "include/ring_buffer.h"
-#include <stdio.h>
+
+struct ring_buffer {
+
+    size_t size, capacity;
+    size_t head, tail;
+    uint64_t *buf;
+
+    void *(*mem_alloc)  (size_t size);
+    void *(*mem_calloc) (size_t blocks, size_t size);
+    void  (*mem_free)   (void *block);
+
+};
 
 enum cc_stat rbuf_new(Rbuf **rbuf)
 {
@@ -18,34 +29,30 @@ enum cc_stat rbuf_new(Rbuf **rbuf)
 
 enum cc_stat rbuf_conf_new(RbufConf *rconf, Rbuf **rbuf)
 {
-    Rbuf *ringbuf = rconf->mem_alloc(sizeof(Rbuf));
+    Rbuf *ringbuf = rconf->mem_calloc(1, sizeof(Rbuf));
     if (!ringbuf)
        return CC_ERR_ALLOC;
-    if (!(ringbuf->buf = rconf->mem_calloc(DEFAULT_RBUF_CAPACITY, sizeof(Rbuf)))) {
+    if (!(ringbuf->buf = rconf->mem_calloc(rconf->capacity, sizeof(Rbuf)))) {
       rconf->mem_free(ringbuf);
       return CC_ERR_ALLOC;
     }
 
-    for (int i = 0; i < DEFAULT_RBUF_CAPACITY; i++)
-        ringbuf->buf[i] = 0x0;
-
     ringbuf->mem_alloc = rconf->mem_alloc;
     ringbuf->mem_calloc = rconf->mem_calloc;
     ringbuf->mem_free = rconf->mem_free;
-    ringbuf->capacity = DEFAULT_RBUF_CAPACITY;
+    ringbuf->capacity = rconf->capacity;
     ringbuf->size = 0;
     ringbuf->head = 0, ringbuf->tail = 0;
     *rbuf = ringbuf;
     return CC_OK;
 }
 
-enum cc_stat rbuf_conf_init(RbufConf *rconf)
+void rbuf_conf_init(RbufConf *rconf)
 {
     rconf->capacity = DEFAULT_RBUF_CAPACITY;
     rconf->mem_alloc = malloc;
     rconf->mem_calloc = calloc;
     rconf->mem_free = free;
-    return CC_OK;
 }
 
 void rbuf_destroy(Rbuf *rbuf)
@@ -54,29 +61,27 @@ void rbuf_destroy(Rbuf *rbuf)
     rbuf->mem_free(rbuf);
 }
 
-int rbuf_is_empty(Rbuf *rbuf)
+bool rbuf_is_empty(Rbuf *rbuf)
 {
     return (rbuf->size == 0);
 }
 
 enum cc_stat rbuf_enqueue(Rbuf *rbuf, uint64_t item)
 {
-    if (!rbuf)
-       return CC_ERR_ALLOC;
     rbuf->buf[rbuf->head] = item;
     rbuf->head = (rbuf->head + 1) % rbuf->capacity;
-    ++rbuf->size;
+    if (rbuf->head == rbuf->tail)
+       rbuf->tail = (rbuf->tail + 1) % rbuf->capacity;
+    if (rbuf->size < rbuf->capacity)
+       ++rbuf->size;
     return CC_OK;
 }
 
 enum cc_stat rbuf_dequeue(Rbuf *rbuf, uint64_t *out)
 {
-    if (!rbuf)
-       return CC_ERR_ALLOC;
     if (rbuf_is_empty(rbuf))
-       return CC_ERR_MAX_CAPACITY;
+       return CC_ERR_OUT_OF_RANGE;
     *out = rbuf->buf[rbuf->tail];
-    rbuf->buf[rbuf->tail] = 0x0;
     rbuf->tail = (rbuf->tail + 1) % rbuf->capacity;
     --rbuf->size;
     return CC_OK;
