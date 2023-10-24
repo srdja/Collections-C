75e6c18afb4550d88c6796b72c01525c1ee30949
JaviMuller <javier.de.muller@tecnico.ulisboa.pt>
2023-10-24 08:24:23 +0100

    Fix #171

--- src/cc_treetable.c
+++ src/cc_treetable.c
@@ -544,6 +544,9 @@ static INLINE void transplant(CC_TreeTable *table, RBNode *u, RBNode *v)
 static INLINE RBNode *tree_min(CC_TreeTable const * const table, RBNode *n)
 {
     RBNode *s = table->sentinel;
+    
+    if (n == s)
+        return s;
 
     while (n->left != s)
         n = n->left;
@@ -553,6 +556,9 @@ static INLINE RBNode *tree_min(CC_TreeTable const * const table, RBNode *n)
 static INLINE RBNode *tree_max(CC_TreeTable const * const table, RBNode *n)
 {
     RBNode *s = table->sentinel;
+    
+    if (n == s)
+        return s;
 
     while (n->right != s)
         n = n->right;
